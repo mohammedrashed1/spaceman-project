d5e35d229d26efffadc35c796f066fef7d550681
mohammedrashed1 <mhmdmqdad112@gmail.com>
2025-12-15 12:23:56 +0300

    fix some errors in checkAnswer function and disable button after select and replace it with the correct letter

--- js/app.js
+++ js/app.js
@@ -28,20 +28,20 @@ const alphabetLtrs = [
 ]
 
 const words = [
-  "gravity",
-  "station",
-  "rocket",
-  "galaxy",
-  "diamond",
-  "sunrise",
-  "electric",
-  "journey",
-  "picture",
-  "monitor"
+  "GRAVITY",
+  "STATION",
+  "ROCKET",
+  "GALAXY",
+  "DIAMOND",
+  "SUNRISE",
+  "ELECTRIC",
+  "JOURNEY",
+  "PICTURE",
+  "MONITOR"
 ]
 const hintH2 = document.querySelector("#h2")
 const displayWord = document.querySelector("#displayWord");
-
+const keys = document.querySelectorAll(".key")
 
 let cpuChoice = Math.floor(Math.random() * 10)
 
@@ -56,29 +56,55 @@ console.log(answer)
 console.log(letters)
 
 function discerption(){
-if(word === "gravity"){
+if(word === "GRAVITY"){
     hint = "Hint: The force that pulls objects toward the ground"
-} else if(word === "station"){
+} else if(word === "STATION"){
     hint = "Hint: A place where people or trains stop"
-} else if(word === "rocket"){
+} else if(word === "ROCKET"){
     hint = "Hint: Something that flies fast into space"
-} else if(word === "galaxy"){
+} else if(word === "GALAXY"){
     hint = "Hint: A huge group of stars and planets"
-} else if(word === "diamond"){
+} else if(word === "DIAMOND"){
     hint = "Hint: A shiny, valuable gemstone"
-} else if(word === "sunrise"){
+} else if(word === "SUNRISE"){
     hint = "Hint: When the sun rises in the morning"
-} else if(word === "electric"){
+} else if(word === "ELECTRIC"){
     hint = "Hint: Something that works with electricity"
-} else if(word === "journey"){
+} else if(word === "JOURNEY"){
     hint = "Hint: A long trip or adventure"
-} else if(word === "picture"){
+} else if(word === "PICTURE"){
     hint = "Hint: An image or a photo you can see"
-} else if(word === "monitor"){
+} else if(word === "MONITOR"){
     hint = "Hint: A screen used for a computer or watching things"
 }
 hintH2.textContent = hint
 }
+
+function checkAnswer() {
+  keys.forEach(button => {
+    button.addEventListener("click", () => {
+      const letter = button.textContent;
+
+      if (letters.includes(letter)) {
+        console.log("Correct Select");
+
+        letters.forEach((ltr, index) => {
+          if (ltr === letter) {
+            answer[index] = letter;
+          }
+        });
+
+        displayWord.textContent = answer.join(" ");
+      } else {
+        console.log("Wrong Select");
+      }
+
+      button.disabled = true;
+    });
+  });
+}
+
+
 displayWord.textContent = answer.join(" ");
 
 
@@ -87,5 +113,5 @@ displayWord.textContent = answer.join(" ");
 
 console.log(word)
 discerption()
-
+checkAnswer()
 
